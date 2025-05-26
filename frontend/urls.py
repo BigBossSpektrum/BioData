0c58de971d1329c4d91ccf1383697f417f543b6f
pythoncga <silvekerhernandez@proton.me>
2025-05-26 18:44:26 -0500

    DELETE VIEW

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,8 +3,5 @@ from . import views  # o los views que uses
 
 urlpatterns = [
     path('', views.home, name='home_biometrico'),
-    path('resumen/', views.tabla_biometrico, name='tabla_biometrico'),
     path('historial_asistencia/', views.historial_asistencia, name='historial_asistencia'),
-    
 ]
-    
--- frontend/views.py
+++ frontend/views.py
@@ -1,41 +1,11 @@
 from django.shortcuts import render
 from API.models import RegistroAsistencia
-from datetime import timedelta
+from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
     return render(request, 'home.html')
-
-def tabla_biometrico(request):
-    registros = RegistroAsistencia.objects.select_related('usuario').order_by('usuario__id', 'timestamp')
-
-    usuarios_registros = {}
-    for registro in registros:
-        usuario = registro.usuario
-
-        if usuario not in usuarios_registros:
-            usuarios_registros[usuario] = {
-                'entrada': None,
-                'salida': None,
-                'tiempo_trabajado': timedelta()
-            }
-
-        # Nueva lógica basada en el turno
-        estado_determinado = determinar_estado_por_turno(usuario, registro.timestamp)
-
-        if estado_determinado == 0:  # Entrada
-            usuarios_registros[usuario]['entrada'] = registro.timestamp
-        elif estado_determinado == 1:  # Salida
-            usuarios_registros[usuario]['salida'] = registro.timestamp
-            if usuarios_registros[usuario]['entrada']:
-                tiempo = registro.timestamp - usuarios_registros[usuario]['entrada']
-                usuarios_registros[usuario]['tiempo_trabajado'] += tiempo
-                usuarios_registros[usuario]['entrada'] = None
-
-    return render(request, 'tabla_biometrico.html', {
-        'usuarios_registros': usuarios_registros
-    })
 
 
 def historial_asistencia(request):
@@ -43,7 +13,7 @@ def historial_asistencia(request):
     return render(request, 'tabla_biometrico.html', {
         'registros': registros
     })
-
+    
 def determinar_estado_por_turno(usuario, timestamp):
     """
     Determina si el registro es entrada (0) o salida (1) según la jornada laboral asignada.
