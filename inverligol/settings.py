65d4306c64f5f91842f8e3d8a4de62764e7b5868
pythoncga <silvekerhernandez@proton.me>
2025-06-02 10:40:20 -0500

    ADDED JAZZMIN ADMINISTRATION
    Added Jazzming administration interface

--- inverligol/settings.py
+++ inverligol/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,14 @@ INSTALLED_APPS = [
     'frontend',
     'accounts',
 ]
+
+JAZZMIN_SETTINGS = {
+    "login_logo": "img/ECYREU.jpg",
+        # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+        # Welcome text on the login screen
+    "welcome_sign": "Bienvenido a Entreredes Access",
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
