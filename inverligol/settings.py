71b10360750cc4e5caa772235dc594741b0e3121
BigBossSpektrum <silvekerhernandez@proton.me>
2025-07-16 14:24:39 -0500

    CHANGE
    Changed DB_NAME from biometrico to Ligol

--- inverligol/settings.py
+++ inverligol/settings.py
@@ -107,18 +107,7 @@ WSGI_APPLICATION = 'inverligol.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),         # Clever Cloud
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', cast=int),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
-    },
-    'local': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'biometrico',         # o tu base local
+        'NAME': 'Ligol',
         'USER': 'root',
         'PASSWORD': 'L0sm3j0r3s!',
         'HOST': 'localhost',
