db7d75c3d1a384b4c42b4d892fd831d343b133a9
BigBossSpektrum <silvekerhernandez@proton.me>
2025-09-11 12:29:02 -0500

    DELETE: Deleted Historial de asistencia from the project

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,7 +4,6 @@ from .views import aprobar_horas_extra, rechazar_horas_extra
 
 urlpatterns = [
     path('', views.home_biometrico, name='home_biometrico'),
-    path('historial_asistencia/', views.historial_asistencia, name='historial_asistencia'),
     path('resumen_asistencias_diarias/', views.resumen_asistencias_diarias, name='resumen_asistencias_diarias'),
     path('reporte_horas/', views.reporte_horas_trabajadas, name='reporte_horas_trabajadas'),
     path('aprobar_horas_extra/<int:usuario_id>/<str:dia>/', aprobar_horas_extra, name='aprobar_horas_extra'),
